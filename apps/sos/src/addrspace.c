3d16fce864fbb35b02dcf170e73d57c897beb94f
Alex Hixon <alex@alexhixon.com>
2013-08-25 20:04:01 +1000

    [M3] Add support for regions in address space.
    Currently uses a link list. Should be using a heap. See issue #4

--- apps/sos/src/addrspace.c
+++ apps/sos/src/addrspace.c
@@ -19,18 +19,11 @@
 #include <sys/panic.h>
 
 #define PAGE_SIZE (1 << seL4_PageBits)
-
-struct as_region REGION_TEST = {seL4_ARM_Default_VMAttributes, seL4_AllRights, 0, 0};
-
-struct as_region*
-as_get_region (addrspace_t as, vaddr_t vaddr) {
-	return &REGION_TEST;
-}
+#define PAGE_MASK (~(PAGE_SIZE - 1))
 
 addrspace_t
 addrspace_create (seL4_ARM_PageTable pd)
 {
-	printf ("mallocing struct\n");
 	addrspace_t as = malloc (sizeof (struct addrspace));
 
 	if (!as) {
@@ -50,7 +43,7 @@ addrspace_create (seL4_ARM_PageTable pd)
 	if (pd != 0) {
 		as->pagedir_cap = pd;
 	} else {
-		printf ("creating new pagedir\n");
+		printf ("!!!! creating new pagedir DO YOU WANT THIS?\n");
 		/* create a new page directory */
 		int err;
 
@@ -93,11 +86,125 @@ as_map_page (addrspace_t as, vaddr_t vaddr) {
 	vaddr &= ~(PAGE_SIZE - 1);
 	printf ("as_map_page: aligned vaddr = 0x%x\n", vaddr);
 
-	struct as_region* reg = as_get_region (as, vaddr);
+	struct as_region* reg = as_get_region_by_addr (as, vaddr);
 	if (!reg) {
-		printf ("as_map_page: vaddr 0x%x does not belong to region in address space\n", vaddr);
+		printf ("as_map_page: vaddr 0x%x does not belong to any region\n", vaddr);
 		return false;
 	}
 
 	return page_map (as, reg, vaddr);
+}
+
+/* Region handling */
+
+int
+as_region_overlaps (addrspace_t as, struct as_region* region_check) {
+     struct as_region* region = as->regions;
+     while (region) {
+     	/* don't check ourselves */
+     	if (region != region_check) {
+			vaddr_t check_base = region_check->vbase;
+			vaddr_t check_limit = check_base + region_check->size;
+
+			vaddr_t reg_base = region->vbase;
+			vaddr_t reg_limit = reg_base + region->size;
+
+			if (check_base < reg_limit && check_limit > reg_base) {
+				return true;
+			}
+		}
+
+		region = region->next;
+     }
+ 
+     return false;
+ }
+
+struct as_region*
+as_get_region_by_addr (addrspace_t as, vaddr_t vaddr) {
+	printf ("looking up region with vaddr 0x%x\n", vaddr);
+
+	struct as_region* region = as->regions;
+
+	/* find a region that contains the fault address */
+	while (region != NULL) {
+		if (vaddr >= region->vbase && vaddr <= (region->vbase + region->size)) {
+			break;
+		}
+
+		region = region->next;
+	}
+
+	return region;
+}
+
+void 
+as_region_insert (addrspace_t as, struct as_region* reg) {
+	struct as_region* last = as->regions;
+     while (last != NULL && last->next != NULL) {
+         last = last->next;
+     }
+ 
+     if (last != NULL) {
+         last->next = reg;
+     } else {
+         as->regions = reg;
+     }
+}
+
+struct as_region*
+as_get_region_by_type (addrspace_t as, as_region_type type) {
+	return as->special_regions[type];
+}
+
+struct as_region*
+as_define_region (addrspace_t as, vaddr_t vbase, size_t size, seL4_CapRights permissions, as_region_type type) {
+	printf ("as_define_region: before alignment: vbase = 0x%x, size = 0x%x\n", vbase, size);
+
+	/* make sure we're page aligned */
+	size += vbase & ~((vaddr_t)PAGE_MASK);
+	vbase &= PAGE_MASK;
+
+	size = (size + PAGE_SIZE - 1) & PAGE_MASK;
+
+	printf ("as_define_region: after alignment:  vbase = 0x%x, size = 0x%x\n", vbase, size);
+
+	if (vbase == 0) {
+		printf ("as_create_region: mapping 0th page is invalid\n");
+		return NULL;
+	}
+
+	struct as_region* reg = malloc (sizeof (struct as_region));
+	if (!reg) {
+		return NULL;
+	}
+
+	reg->vbase = vbase;
+	reg->size = size;
+	reg->permissions = permissions;
+	//reg->type = type;
+
+	if (as_region_overlaps (as, reg)) {
+		free (reg);
+		return NULL;
+	}
+
+	as_region_insert (as, reg);
+
+	if (type != REGION_GENERIC) {
+		as->special_regions[type] = reg;
+	}
+
+	return reg;
+}
+
+struct as_region*
+as_resize_region (addrspace_t as, struct as_region* reg, size_t amount) {
+	reg->size += amount;
+	if (as_region_overlaps (as, reg)) {
+		reg->size -= amount;
+		return NULL;
+	}
+
+	return reg;
 }
--- apps/sos/src/addrspace.h
+++ apps/sos/src/addrspace.h
@@ -3,11 +3,20 @@
 #ifndef __ADDRSPACE_H__
 #define __ADDRSPACE_H__
 
+#define NUM_SPECIAL_REGION_TYPES	3
+typedef enum {
+	REGION_STACK,
+	REGION_HEAP,
+	REGION_IPC,
+	REGION_GENERIC,
+} as_region_type;
+
 struct addrspace {
 	seL4_ARM_PageDirectory pagedir_cap;
 	seL4_Word pagedir_addr;
 
-	struct region* regions;
+	struct as_region* regions;
+	struct as_region* special_regions[NUM_SPECIAL_REGION_TYPES];
 	pagetable_t pagetable;
 };
 
@@ -17,8 +26,10 @@ struct as_region {
 	seL4_ARM_VMAttributes attributes;
 	seL4_CapRights permissions;
 
-	vaddr_t from;
-	vaddr_t to;
+	vaddr_t vbase;
+	size_t size;
+
+	struct as_region* next;
 };
 
 addrspace_t
@@ -26,5 +37,17 @@ addrspace_create (seL4_ARM_PageTable pd);
 
 int
 as_map_page (addrspace_t as, vaddr_t vaddr);
+
+struct as_region*
+as_resize_region (addrspace_t as, struct as_region* reg, size_t amount);
+
+struct as_region*
+as_define_region (addrspace_t as, vaddr_t vbase, size_t size, seL4_CapRights permissions, as_region_type type);
+
+struct as_region*
+as_get_region_by_type (addrspace_t as, as_region_type type);
+
+struct as_region*
+as_get_region_by_addr (addrspace_t as, vaddr_t start);
 
 #endif
