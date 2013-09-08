f0589e8dd6eedd0fba4f0495b1c00ca5721738d8
Alex Hixon <alex@alexhixon.com>
2013-09-08 14:52:38 +1000

    Forgot to add network include file for other apps to use.
    --HG--
    branch : seedless

---
+++ libs/libsos/include/network.h
@@ -0,0 +1,10 @@
+#ifndef __NETWORK_H__
+#define __NETWORK_H__
+
+#define NETSVC_SERVICE_REGISTER		(1)
+#define NETSVC_SERVICE_UNREGISTER	(2)
+
+#define NETSVC_PROTOCOL_UDP			(1)
+#define NETSVC_PROTOCOL_TCP			(2)
+
+#endif
