9a30c8e4c560e4eac4a2b062bab5fe658cc4d357
Alex Hixon <alex@alexhixon.com>
2013-11-07 22:17:31 +1100

    Uhhh, always flush BEFORE closing. Nice one, whoever wrote fclose.c.
    --HG--
    branch : seedless

--- libs/libsel4c/src/fclose.c
+++ libs/libsel4c/src/fclose.c
@@ -8,8 +8,8 @@ fclose(FILE *stream)
 {
 	int r;
 	lock_stream(stream);
-	r = stream->close_fn(stream->handle);
 	fflush(stream);
+	r = stream->close_fn(stream->handle);
 	unlock_stream(stream);
 	return r;
 }
