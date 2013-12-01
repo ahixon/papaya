f829c5025c6304a7a608daf7aae415d4c372178d
Alex Hixon <alex.hixon@nicta.com.au>
2013-12-01 16:21:12 +1100

    Cleanup build system.
    --HG--
    branch : seedless

--- libs/libserial/include/serial.h
+++
@@ -1,4 +0,0 @@
-extern struct serial *serial_init(void);
-extern int serial_send(struct serial *serial, char *data, int len);
-extern int serial_register_handler(struct serial *serial, 
-			void (*handler) (struct serial *serial, char c));
